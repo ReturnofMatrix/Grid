769bcaacaf8cb9a7e7ed206ae6e11af733a3e2e7
ReturnofMatrix <roshanbhogan007@gmail.com>
2024-08-15 12:19:16 +0530

    You did the javascript function create grid with evenetListneres of click and keypress and then box divs are added with dynamic container size.

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const container = document.querySelector("div");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
-for (let i = 0; i < 256; i++) { // 16 * 16 = 256 squares
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+function createGrid(){
+    container.textContent='';
+    let num=parseInt(input.value);
+
+    if(num>100){
+        alert("Enter number lower than 100.");
+        return;
+    }
+
+    input.value='';
+    container.style.width=`${num*20}px`;
+    container.style.height= `${num*20}px`;
+
+    for(let i=0; i<num*num; i++){
+        const box=document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);
+    }
 }
+
+button.addEventListener("click",createGrid);
+
+input.addEventListener("keypress",(event)=>{
+    if(event.key==="Enter"){
+        createGrid();
+    }
+});
